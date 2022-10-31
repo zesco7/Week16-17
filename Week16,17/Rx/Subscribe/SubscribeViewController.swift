1090885b45e18c4b9041287005e3c1ef1d61820c
hello <hello>
2022-10-31 19:03:06 +0900

    080복습(RxDataSources, RxAlamofire)

--- Week16,17/Rx/Subscribe/SubscribeViewController.swift
+++ Week16,17/Rx/Subscribe/SubscribeViewController.swift
@@ -7,18 +7,45 @@
 
 import UIKit
 
+import RxAlamofire
 import RxCocoa
+import RxDataSources
 import RxSwift
 
 class SubscribeViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     let disposeBag = DisposeBag()
     
+    //데이터 재사용: 데이터소스, 표시할 테이블뷰, 셀위치, 셀에 표현할 데이터를 클로저로 넘김(diffable처리방식과 비슷하게 전역변수로 선언)
+    lazy var dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Int>> (configureCell: { dataSource, tableView, indexPath, item in
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = "\(item)" //행 내용 표시
+        return cell
+    })
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testAlamofire()
+        textRxDataSource()
+        
+        Observable.of(1,2,3,4,5,6,7,8,9,10)
+            .skip(3) //3까지 스킵하고 4부터 실행 (4~10)
+            .debug() //디버그 사용하면 print로 일일이 안찍어봐도 됨(4~10)
+            .filter { $0 % 2 == 0 } //(4,6,8,10)
+            .map { $0 * 2 } //(8,12,16,20)
+            .subscribe { value in
+                print("===\(value)")
+            }
+            .disposed(by: disposeBag)
+        
+        let buttonType = button
+        let rxType = button.rx
+        let controlEventType = button.rx.tap
         
         //1.
         button.rx.tap
@@ -68,5 +95,36 @@ class SubscribeViewController: UIViewController {
             .disposed(by: disposeBag)
     }
     
-
+    //Rx사용하여 네트워크 통신
+    func testAlamofire() {
+        //통신성공, 실패 두가지 경우만 있음: 네트워크객체 대응할 수 있는 single이 있음(driver같은 것)
+        let url = APIKey.searchURL + "apple"
+        request(.get, url, headers: ["Authorization": APIKey.authorization]) //requset(통신방식, 통신요청할URL, 헤더)
+            .data() //응답내용을 데이터 타입으로 변경
+            .decode(type: SearchPhoto.self, decoder: JSONDecoder()) //받은 데이터를 디코딩
+            .subscribe { value in
+                print(value)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    //Rx사용하여 테이블뷰 section, row 생성
+    func textRxDataSource() {
+        //테이블뷰 등록
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        //헤더 내용 표시(bind보다 먼저 선언해줘야 함)
+        dataSource.titleForHeaderInSection = { dataSource, index in
+            return dataSource.sectionModels[index].model
+        }
+        
+        //재사용할 데이터 설정
+        Observable.just([
+            SectionModel(model: "title", items: [1, 2, 3]),
+            SectionModel(model: "title", items: [1, 2, 3]),
+            SectionModel(model: "title", items: [1, 2, 3]),
+        ])
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+    }
 }
